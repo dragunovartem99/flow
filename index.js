4f73b54d114b878e34abc863de88a4b1c22511dd
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-23 16:06:24 +0300

    Make the tool CLI-friendly

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+#!/usr/bin/env node
+
+import path from "path";
 import { Flow } from "./objects/Flow.js";
 import { Config } from "./objects/config/Config.js";
 
-new Flow(new Config(".flow.json").parse()).start();
+const cwd = process.cwd();
+
+try {
+	const configPath = path.join(cwd, ".flow.json");
+	new Flow(new Config(configPath).parse()).start();
+} catch (error) {
+	console.error(error.toString());
+	process.exit(1);
+}
